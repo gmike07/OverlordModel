08c79872aaf8d7483a4425fac7ef6231f1f15c55
lenechk <mikegreenbaum1@gmail.com>
2020-10-26 19:25:21 +0200

    Update training.py

--- overlord/network/training.py
+++ overlord/network/training.py
@@ -156,8 +156,8 @@ class Model:
 
 		summary = SummaryWriter(log_dir=tensorboard_dir)
 		epochs = self.config['train']['n_epochs']
-		if os.path.exists(checkpoint_dir) and os.path.exists(os.path.join(checkpoint_dir, 'objs.pkl')):
-			objs = pickle.load(os.path.join(checkpoint_dir, 'objs.pkl'), 'rb')
+		if os.path.exists(model_dir) and os.path.exists(os.path.join(model_dir, 'objs.pkl')):
+			objs = pickle.load(os.path.join(model_dir, 'objs.pkl'), 'rb')
 			epochs, optimizer, scheduler = objs['epochs'], objs['optimizer'], objs['scheduler']
 		for epoch in range(self.config['train']['n_epochs']):
 			self.latent_model.train()
@@ -199,9 +199,9 @@ class Model:
 				score_train, score_test = self.classification_score(X=content_codes, y=classes)
 				summary.add_scalar(tag='class_from_content/train', scalar_value=score_train, global_step=epoch)
 				summary.add_scalar(tag='class_from_content/test', scalar_value=score_test, global_step=epoch)
-				if os.path.exists(checkpoint_dir):
+				if os.path.exists(model_dir):
 					objs = {'epochs': epoch, 'optimizer': optimizer, 'scheduler': scheduler}
-					with open(os.path.join(checkpoint_dir, 'objs.pkl'), 'wb') as f:
+					with open(os.path.join(model_dir, 'objs.pkl'), 'wb') as f:
 						pickle.dump(objs, f)
 			self.save(model_dir)
 
