fe690c5a84824d1139255bac579ada0bf3ed2e13
lenechk <mikegreenbaum1@gmail.com>
2020-11-25 18:16:18 +0200

    Update utils.py

--- overlord/network/utils.py
+++ overlord/network/utils.py
@@ -39,3 +39,22 @@ class AugmentedDataset(NamedTensorDataset):
 				item['img_augmented'][i] = self.__transform(item['img'][i])
 
 		return item
+
+	
+class AverageMeter:
+
+	def __init__(self):
+		self.reset()
+
+	def reset(self):
+		self.val = 0
+		self.avg = 0
+		self.sum = 0
+		self.count = 0
+
+	def update(self, val, n=1):
+		self.val = val
+		self.sum += val * n
+		self.count += n
+		self.avg = self.sum / self.count
+
